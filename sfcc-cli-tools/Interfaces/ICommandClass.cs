a1ce0bb35dfba001deb1666b96c83815be8f66a3
Galen Goforth <ggoforth@pixelmedia.com>
2019-12-15 00:14:04 -0500

    Command - impex: - Updated Help & Impex command classes with 2nd PrintHelp method. - Began processing logic for ProcessOption of the sftools impex command.

--- sfcc-cli-tools/Interfaces/ICommandClass.cs
+++ sfcc-cli-tools/Interfaces/ICommandClass.cs
@@ -10,13 +10,15 @@ namespace sfcc_cli_tools.Interfaces
         /// <summary>
         ///     Processes the specified option with the passed argument values.
         /// </summary>
-        /// <param name="optionName"></param>
+        /// <param name="optionName">The name of the option specified in the CLI.</param>
+        /// <param name="args">Any argument values that are specified for the cammand that is issued.</param>
         /// <returns>
         ///     Returns a success flag indicating if the operation was
         ///     successful or not.
         /// </returns>
-        bool ProcessOption(string optionName, string[] arguments);
+        bool ProcessOption(string optionName, string[] args);
         void Default();
         void PrintHelp();
+        void PrintHelp(string subCommand);
     }
 }
